b08a6fb65a1339ed4c58b71447ca05fdd1d718db
jiangliushen <44798204+jiangliushen@users.noreply.github.com>
2019-04-08 16:14:12 +0800

    Add files via upload

---
+++ TrackSpoolgen/TrackSpoolgen/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TrackSpoolgen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.read = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pgb = new System.Windows.Forms.ProgressBar();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // read
+            // 
+            this.read.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.read.Location = new System.Drawing.Point(520, 27);
+            this.read.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(98, 32);
+            this.read.TabIndex = 4;
+            this.read.Text = "读取并导出";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(131, 30);
+            this.textBoxPath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(360, 26);
+            this.textBoxPath.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "读取文件夹:";
+            // 
+            // pgb
+            // 
+            this.pgb.Location = new System.Drawing.Point(108, 278);
+            this.pgb.Name = "pgb";
+            this.pgb.Size = new System.Drawing.Size(400, 17);
+            this.pgb.TabIndex = 8;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // rtb
+            // 
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rtb.Location = new System.Drawing.Point(131, 94);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(360, 120);
+            this.rtb.TabIndex = 11;
+            this.rtb.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "读取文件名：";
+            // 
+            // txtCount
+            // 
+            this.txtCount.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCount.Location = new System.Drawing.Point(416, 235);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(146, 19);
+            this.txtCount.TabIndex = 13;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(497, 186);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(144, 28);
+            this.btnCheck.TabIndex = 14;
+            this.btnCheck.Text = "查看TrackingList";
+            this.btnCheck.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "进度条:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(653, 316);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.pgb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.read);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "SpoolgenExcel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pgb;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TrackSpoolgen/TrackSpoolgen/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Cells;
+
+
+namespace TrackSpoolgen
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //模板文件相对路径
+        //string Template_File_Path = @"..\..\Template\Template.xlsx";
+        string Template_File_Path = @"Template\Template.xlsx";//模板相对路径
+        string strDesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);//桌面路径
+        int i = 1;
+
+        private void read_Click(object sender, EventArgs e)
+        {            
+            //  打开 Excel 模板
+            Workbook TemplateWorkbook = File.Exists(Template_File_Path) ? new Workbook(Template_File_Path) : new Workbook();
+            //  打开模板第一个sheet
+            Worksheet TemplateSheet = TemplateWorkbook.Worksheets[0];
+            Cells cells0 = TemplateSheet.Cells;
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();//打开文件夹对话框
+            if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                string FolderPath= dialog.SelectedPath;
+                textBoxPath.Text = FolderPath;
+                
+                DirectoryInfo root = new DirectoryInfo(FolderPath);
+                FileInfo[] files1 = root.GetFiles();
+                
+                int fileCount = 0;
+                foreach (FileInfo file in files1)
+                {
+                    string fileName = file.Name;
+                    string fileFullName = file.FullName;
+                   
+                    if (fileName.Contains("Material"))
+                    {
+                        fileCount++;
+                        rtb.AppendText(fileName+"\n");//将文件名添加至textbox中   
+                        
+                        HandleMaterial(fileFullName, cells0);                      
+                      
+                    }                  
+                    txtCount.Text = "共读取到"+" "+fileCount.ToString()+ " " + "个文件";
+                }
+               
+                TemplateWorkbook.Save(strDesktopPath+@"\TrackingList.xlsx");                
+                MessageBox.Show("TrackingList： 已完成", "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+        
+        private void HandleMaterial(string fileFullName,Cells cells0)
+        {
+            string line = string.Empty;
+            StreamReader reader = new StreamReader(fileFullName, Encoding.Default);
+            //int i = 1;
+            
+            while ((line = reader.ReadLine()) != null)
+            {
+                
+                string col1 = line.Substring(0, 10).Trim();
+                if(col1=="SPCCLASS")
+                {
+                    continue;
+                }
+                string col2 = line.Substring(10, 50).Trim();
+                string col3 = line.Substring(60, 30).Trim();
+                string col4 = line.Substring(90, 30).Trim();
+                string col5 = line.Substring(120, 30).Trim();
+                string col6 = line.Substring(150, 30).Trim();
+                string col7 = line.Substring(180, 30).Trim();
+                string col8 = line.Substring(210, 30).Trim();
+                string col9 = line.Substring(240, 30).Trim();
+                string col10 = line.Substring(270, 30).Trim();
+                string col11 = line.Substring(300, 31).Trim();
+                string col12 = line.Substring(331, 29).Trim();
+                string col13 = line.Substring(360, 30).Trim();
+                string col14 = line.Substring(390, 29).Trim();
+                string col15 = line.Substring(419, 31).Trim();
+                string col16 = line.Substring(450, 30).Trim();
+                string col17 = line.Substring(480, 30).Trim();
+                string col18 = line.Substring(510, 30).Trim();
+                string col19 = line.Substring(540, 30).Trim();
+                string col20 = line.Substring(570, 30).Trim();
+                string col21 = line.Substring(600, 31).Trim();
+                string col22 = line.Substring(630, 29).Trim();
+                string col23 = line.Substring(660, 30).Trim();
+                string col24 = line.Substring(690, 29).Trim();
+                string col25 = line.Substring(720, 30).Trim();
+                string col26 = line.Substring(750, 30).Trim();
+                string col27 = line.Substring(780, 30).Trim();
+                string col28 = line.Substring(810, 140).Trim();
+                string col29 = line.Substring(950, line.Length-950).Trim();
+                
+                //cell和表格的对应关系是-1的关系
+                cells0[i, 15 - 1].PutValue(col2);
+                cells0[i, 16 - 1].PutValue(col4);
+                cells0[i, 23 - 1].PutValue(col6);
+                cells0[i, 22 - 1].PutValue(col6);
+                if (col9.Contains("MM"))
+                {
+                    string[] unitArry9 = col9.Split(' ');
+                    cells0[i, 39 - 1].PutValue(unitArry9[1]);
+                    cells0[i, 40 - 1].PutValue(unitArry9[0]);
+                    cells0[i, 42 - 1].PutValue(unitArry9[0]);
+                }                  
+                else                   
+                {
+                    cells0[i, 38 - 1].PutValue(col9);
+                    cells0[i, 39 - 1].PutValue("Pcs");//单位
+                }
+                //string[] arry=cells[i, 14 - 1].Value.ToString().Split('x');//待定x的形式，x不存在问题
+                //cells0[i, 28 - 1].PutValue(arry[0]);
+                cells0[i, 28 - 1].PutValue(col14);
+                //cells0[i, 29 - 1].PutValue(arry[1]);
+                cells0[i, 27 - 1].PutValue(col15);
+                cells0[i, 57 - 1].PutValue(col16);
+                cells0[i, 10 - 1].PutValue(col19);
+                cells0[i, 18 - 1].PutValue(col25);
+                cells0[i, 17 - 1].PutValue(col26);
+                cells0[i, 32 - 1].PutValue(col27);
+                cells0[i, 52 - 1].PutValue(col28);
+                cells0[i, 69 - 1].PutValue(col29);
+                i++;
+
+                pgb.Minimum = 0;//进度条
+                pgb.Maximum = i - 1;//最大值
+                pgb.Step = 1;
+                pgb.PerformStep();// 使用performstep方法按Step值递增
+                
+            }          
+
+        }
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(strDesktopPath + @"\TrackingList.xlsx");//打开桌面路径下的TrackingList
+
+        }           
+    }
+}
